60502d445e00dc5353409fb971ad57566040bad3
amogh7joshi <joshi.amoghn@gmail.com>
2021-02-18 14:01:03 -0500

    Removed testing files

--- test.py
+++
@@ -1,5 +0,0 @@
-from nations.info import nation_flag_info
-
-with nation_flag_info as info:
-   for item in info.keys():
-      print(info.keys())
--- wiki-test.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-print(len(os.listdir('./nations/flag_image_storage')))
